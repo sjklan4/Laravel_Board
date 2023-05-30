48a79779d8ee9c7f473f2e298434bc1c9c37cdc4
sjklan4 <dpeltms4@gmail.com>
2023-05-30 18:24:29 +0900

    0530

---
+++ board/resources/views/login.blade.php
@@ -0,0 +1,25 @@
+{{-- 부모값 받아오기 --}}
+@extends('layout.layout') 
+
+
+@section('title', 'Login')
+
+@section('contents')
+    <h1>LOGIN</h1>
+    @include('layout.errorsvalidate')
+
+    <div>{{session('success')?? '이건가??'}}</div>
+    
+    <form action="{{route('users.login.post')}}" method="post">
+        @csrf
+        <label for="email">Email : </label>
+        <input type="text" name="email" id="email">
+
+        <label for="password">password : </label>
+        <input type="password" name="password" id="password">
+        <br>
+        <button type = "submit">Login</button>
+        <button type = "button" onclick="location.href='{{route('users.registration')}}'">회원가입</button>
+    </form>
+
+@endsection
